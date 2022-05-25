ca6d1b3c9d1bbe31b82a56a86f46d835fe01cea4
lelotok <98822210+lelotok@users.noreply.github.com>
2022-05-25 13:31:31 +0200

    Read me file and app fix

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# challenge-mole
+# Project deep Learning: Mole prediction App
+
+### By [Lelo Tokwaulu](www.linkedin.com/in/lelotokwaulu)
+--------------------------------------------------------
+## Description 
+
+Creating an App that will predict mole type using just a image of a mole.
+
+## Modules used
+
+- Python
+- Pandas
+- Numpy
+- tensorflow
+- Keras
+- os
+- pathlib
+- Sklearn
+- Flask
+- matplotlib
+
+## Installation
+
+1. Clone the repo
+2. Install the libraries (check requirements.txt)
+3. Run app.py
+
+## Starting and running
+
+1. Open app in a browser
+2. upload an mole img/png
+3. Click button 'Predict'
+
+## Output
+
+A text ('Mole type is: ') will pop up with mole type.
+
+## Data
+
+Model trained based on Skin cancer data from Kaggle 
+https://www.kaggle.com/kmader/skin-cancer-mnist-ham10000
+
+## Model
+
+Used a Keras pre-trained model 'mobilenet_v2' to train the dataset. Maybe need to test other pre-trained for better accuracy.
+
+## Note:
+
+This is first version, model need to be refined as the app design. 
+
+## Acknowledges
+
+Thank you!
+
--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
 app = Flask(__name__)
 
+model = load_model("assets/mole_model.h5")
 
 @app.route('/')
 def home():
@@ -16,33 +17,36 @@ def home():
     
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    model = load_model("assets/mole_model.h5")
+    img = request.files['mole_img']
+    img.save(img.name)
+    
 
     # Load the image
     image_size = (224,224)
-    img = image.load_img(request.files['mole_img'], target_size= image_size)
-
+    img = image.load_img(img.name, target_size= image_size)
+    print('hello')
     # Preprocess the image (as in the training set)
     img_array = image.img_to_array(img)
     img_array = img_array.reshape((1, img_array.shape[0], img_array.shape[1], img_array.shape[2]))
     prepared_img = preprocess_input(img_array)
     # Apply the model to get the prediction
-
-    prediction = model.predict(prepared_img)
-
+    print('Go for prediction')
+    prediction = model.predict(prepared_img)[0]
+    print(prediction)
     # Interpret the prediction
-    max = prediction.max()
-    idx = np.where( prediction==max)
+    #max = prediction.max()
+    idx = int(np.argmax(prediction))
     
     # Display the prediction
-    labels = ['nv', 'bkl', 'mel', 'akiec', 'bcc', 'vasc', 'df'],
+    labels = ['nv', 'bkl', 'mel', 'akiec', 'bcc', 'vasc', 'df']
     # You are done ! Congrats :)
-
+    print(idx)
     output = labels[idx]
+    
 
     print(output)
 
     return render_template('index.html', prediction_text='Mole is type: {}'.format(output))
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
